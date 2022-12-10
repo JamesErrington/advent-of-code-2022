b44707c8dfb7d193ef6a019db67409bf123054af
James Errington <james.errington@secretspa.co.uk>
2022-12-10 19:04:03 +0000

    Day 4

---
+++ 04/part1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./04/question.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	count := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ",")
+		left, right := strings.Split(line[0], "-"), strings.Split(line[1], "-")
+
+		ll, lu, rl, ru := parse_bounds(left, right)
+		if (ll <= rl && lu >= ru) || (rl <= ll && ru >= lu) {
+			count += 1
+		}
+	}
+
+	fmt.Println(count)
+}
+
+func parse_bounds(left []string, right []string) (int, int, int, int) {
+	ll, _ := strconv.Atoi(left[0])
+	lu, _ := strconv.Atoi(left[1])
+	rl, _ := strconv.Atoi(right[0])
+	ru, _ := strconv.Atoi(right[1])
+
+	return ll, lu, rl, ru
+}
